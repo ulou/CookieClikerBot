072b1046e5836697490aa5c8070cc7bd0cf4c4b3
Marsel <ulou@onet.eu>
2015-12-24 17:18:19 +0100

    Start/Stop button appears on webpage

--- CoockieClicker.js
+++ CoockieClicker.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Bot who plays CoockieClicker for you!
 // @author       https://github.com/ulou
-// @match        http://orteil.dashnet.org/cookieclicker/
+// @include        http://orteil.dashnet.org/cookieclicker/
 // @grant        none
 // ==/UserScript==
 /* jshint -W097 */
@@ -20,22 +20,24 @@ function addJQueryAndBegin(){
         var scriptTag = document.createElement("script");
         scriptTag.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js";
         scriptTag.onload = function(){
-            // do smt on load
+            botStart();
         };
         document.body.appendChild(scriptTag);
     } else {
-        // do smt
+        botStart();
     }
 }
 
 function botStart(){
     bot_running = true;
     console.log("Bot Started.");
+    $getToggleBot().text("Start Bot");
 }
 
 function botStop(){
     bot_running = false;
     console.log("Bot Stoped.");
+    $getToggleBot().text("Stop Bot");
 }
 
 function $getToggleBot(){
@@ -44,13 +46,19 @@ function $getToggleBot(){
     if($toggle.length === 0) {
         $toggle = $("<a id='botToggle'>Toggle Bot</a>");
         $toggle.on("click", function(){
-           if(bot_running){
-               botStop();
-           } else{
-               botStart();
-           }
+            if(bot_running){
+                botStop();
+            } else{
+                botStart();
+            }
         });
+        $toggle.css("cursor", "pointer");
+        $toggle.insertBefore("#topBar #links");
+//        $toggle.insertAfter("#versionNumber");
     }
 
     return $toggle;
 }
+
+// init bot
+addJQueryAndBegin();
